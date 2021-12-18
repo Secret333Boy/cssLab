581cf242cbce47572e626e3960666268cf555b2d
Secret333Boy <pashkovskiy.eugen@gmail.com>
2021-12-18 17:39:36 +0200

    Swiper done

--- src/Swiper/Swiper.jsx
+++ src/Swiper/Swiper.jsx
@@ -1,15 +1,38 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import './Swiper.scss';
 import arrowImage from './img/arrow.png';
 
 export function Swiper({ children }) {
+  const [active, setActive] = useState(0);
+  const nodesRef = useRef(null);
+  const nodes = nodesRef.current;
+  console.log(active);
+  nodes?.children[active].classList.add('active');
+  const cardsCount = children.length;
   return (
     <div className="Swiper">
-      <div className="leftArrow" onClick={() => {}}>
+      <div
+        className="leftArrow"
+        onClick={(e) => {
+          console.log(e);
+          nodes?.children[active].classList.remove('active');
+          setActive(active - 1 < 0 ? cardsCount - 1 : active - 1);
+        }}
+      >
         <img src={arrowImage} />
       </div>
-      <div className="nodes">{children}</div>
-      <div className="rightArrow">
+      <div className="nodes" ref={nodesRef}>
+        {children}
+      </div>
+      <div
+        className="rightArrow"
+        onClick={(e) => {
+          e.target.parentElement.previousSibling.children[
+            active
+          ].classList.remove('active');
+          setActive(active + 1 < cardsCount ? active + 1 : 0);
+        }}
+      >
         <img src={arrowImage} />
       </div>
       <div className="swiperStatus" onClick={() => {}}></div>
